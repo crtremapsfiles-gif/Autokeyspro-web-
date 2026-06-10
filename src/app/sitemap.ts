497fd9914c6db96f2d47d08730eaf3363f2f9944
crtremapsfiles-gif <crtremapsfiles@gmail.com>
2026-06-11 01:23:09 +0200

    Add files via upload

---
+++ src/app/sitemap.ts
@@ -0,0 +1,2 @@
+import { MetadataRoute } from 'next'
+export default function sitemap(): MetadataRoute.Sitemap { return [{ url:'https://autokeyspro.es', lastModified:new Date(), changeFrequency:'weekly', priority:1 }] }
